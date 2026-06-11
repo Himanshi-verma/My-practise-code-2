848716ba728c0512f6c2a9d4a44ec4f04de90a5f
Himanshi-verma <himanshivermma@gmail.com>
2026-06-11 22:42:46 +0530

    questions
    mixed function , array, strings questions

--- JS/java script/strings/ques.js
+++ JS/java script/strings/ques.js
@@ -20,31 +20,64 @@
 // console.log(count);
 
 //palindrome or not
-let strr="racecar";
-let step1= strr.split("");
-let step2= step1.reverse();
-let step3= step2.join("");
-if( strr===step3){
-    console.log(`${strr} is a palindrome`);
-}
-else{
-    console.log("it is not a palindrome");
-}
+// let strr="racecar";
+// let step1= strr.split("");
+// let step2= step1.reverse();
+// let step3= step2.join("");
+// if( strr===step3){
+//     console.log(`${strr} is a palindrome`);
+// }
+// else{
+//     console.log("it is not a palindrome");
+// }
 
-//first letter in uppercase
-let letter="uttar pradesh";
-let sp= letter.split(" ");
-let value=sp.map(function(value){
-    return value[0].toUpperCase() + value.slice(1);
-})
-let result=value.join(" ");
-console.log(result);
+// //first letter in uppercase
+// let letter="uttar pradesh";
+// let sp= letter.split(" ");
+// let value=sp.map(function(value){
+//     return value[0].toUpperCase() + value.slice(1);
+// })
+// let result=value.join(" ");
+// console.log(result);
   
 //array of string alphabatically sort
-let arr=["grapes","banana","eagle"];
-let result1=arr.sort();
-console.log(result1)
+// let arr=["grapes","banana","eagle"];
+// let result1=arr.sort();
+// console.log(result1)
 
-//find out most repeated letter in a string
-let str="javascript";
-let max_count=
+//even and odd find
+let arr = [1,5,8,7,6,3,4]
+let even=arr.filter(function(even){
+    return even%2==0; 
+})    
+let odd=arr.filter(function(odd){
+    return odd%2!=0;
+})   
+console.log("even:",even);
+console.log("odd:", odd);
+
+
+//pig latin 
+let str="hello hillo";
+let result=str.split(" ")
+.map(function(pig){
+    return pig.slice(1)+pig[0]+"ay"
+})
+.join(" ")
+console.log(result);
+
+//how many words in a string
+let a="hell my name is himanshi";
+console.log(a.split(" ").length);
+
+//how many letters in a string 
+let b="himanshi";
+console.log(b.split("").length);
+
+//reverse the words 
+let ab="shalu aalu";
+let rev=ab.split(" ")
+.reverse()
+.join(" ");
+console.log(rev);
+
--- JS/java script/strings/tempCodeRunnerFile.js
+++ JS/java script/strings/tempCodeRunnerFile.js
@@ -1,2 +1,19 @@
-everse=a.split("").reverse().join("");
-// console.log(reverse);
+et strr="racecar";
+// let step1= strr.split("");
+// let step2= step1.reverse();
+// let step3= step2.join("");
+// if( strr===step3){
+//     console.log(`${strr} is a palindrome`);
+// }
+// else{
+//     console.log("it is not a palindrome");
+// }
+
+// //first letter in uppercase
+// let letter="uttar pradesh";
+// let sp= letter.split(" ");
+// let value=sp.map(function(value){
+//     return value[0].toUpperCase() + value.slice(1);
+// })
+// let result=value.join(" ");
+// console.log(result);
