a9609ba59f3fc550f16aa8825fcc3c337194696d
Himanshi-verma <himanshivermma@gmail.com>
2026-06-27 23:21:02 +0530

    props

--- React/props/src/App.jsx
+++ React/props/src/App.jsx
@@ -5,12 +5,10 @@ function App() {
   return (
     <div className='father'>  
     
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-    
-    
+        <Card user='Jhon' age={18} img='https://tse3.mm.bing.net/th/id/OIP.o_Qi916FlohRm_fHXACMFQHaEK?pid=Api&P=0&h=180' />
+        <Card user='Harris' age={38} img='https://images.unsplash.com/photo-1781813377841-a3a25348ed17?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D'/>
+        <Card user='Henary' age={28} img='https://plus.unsplash.com/premium_photo-1764532087829-0f652beb1f52?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D'/>
+      
     
     </div>
     
--- React/props/src/components/card.jsx
+++ React/props/src/components/card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function card() {
+function card(props) {
+  // console.log(props.user , props.age)
   return (
     <div className='parent'>
-        <img  src='https://tse3.mm.bing.net/th/id/OIP.o_Qi916FlohRm_fHXACMFQHaEK?pid=Api&P=0&h=180' alt=''></img>
-        <h1>Shinchan</h1>
+        <img  src={props.img} alt=''></img>
+        <h1>{props.user},{props.age}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <button>view profile</button>
     </div>
