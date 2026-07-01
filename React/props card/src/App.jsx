b97e1a75e1267e502139480f67936b354492d3d4
Himanshi-verma <himanshivermma@gmail.com>
2026-07-02 00:03:13 +0530

    Update App.jsx

--- React/props card/src/App.jsx
+++ React/props card/src/App.jsx
@@ -6,7 +6,7 @@ import Card from './components/card'
 function App() {
   const jobs = [
   {
-    logo: "https://logo.clearbit.com/google.com",
+    logo: "https://tse3.mm.bing.net/th/id/OIP.Kg2FF2wpIK_HLyo8Q56ycAHaFj?pid=Api&P=0&h=180",
     company: "Google",
     deadline: "5 days ago",
     position: "Frontend Developer",
@@ -16,7 +16,7 @@ function App() {
     location: "Mountain View, USA",
   },
   {
-    logo: "https://logo.clearbit.com/microsoft.com",
+    logo: "https://tse2.mm.bing.net/th/id/OIP.wAKg-Fwc3o_qIuGNQEk5lgHaFY?pid=Api&P=0&h=180",
     company: "Microsoft",
     deadline: "2 days ago",
     position: "Software Engineer",
@@ -26,7 +26,7 @@ function App() {
     location: "Redmond, USA",
   },
   {
-    logo: "https://logo.clearbit.com/apple.com",
+    logo: "https://tse4.mm.bing.net/th/id/OIP.JM19VAZ_Zne9RxsnEpUe5gHaHa?pid=Api&P=0&h=180",
     company: "Apple",
     deadline: "1 week ago",
     position: "UI/UX Designer",
@@ -36,7 +36,7 @@ function App() {
     location: "Cupertino, USA",
   },
   {
-    logo: "https://logo.clearbit.com/amazon.com",
+    logo: "https://tse2.mm.bing.net/th/id/OIP.qWOzHLmRCs0WdNB0VUL6TAHaHa?pid=Api&P=0&h=180",
     company: "Amazon",
     deadline: "3 days ago",
     position: "Backend Developer",
@@ -46,7 +46,7 @@ function App() {
     location: "Seattle, USA",
   },
   {
-    logo: "https://logo.clearbit.com/meta.com",
+    logo: "https://tse3.mm.bing.net/th/id/OIP.zMUV7B749XG0H15ZaBO_LwHaHa?pid=Api&P=0&h=180",
     company: "Meta",
     deadline: "Today",
     position: "React Developer",
@@ -56,7 +56,7 @@ function App() {
     location: "Menlo Park, USA",
   },
   {
-    logo: "https://logo.clearbit.com/netflix.com",
+    logo: "https://tse2.mm.bing.net/th/id/OIP.LJ1xF1q8b17RBPTHscbX8AHaEK?pid=Api&P=0&h=180",
     company: "Netflix",
     deadline: "4 days ago",
     position: "DevOps Engineer",
@@ -66,7 +66,7 @@ function App() {
     location: "Los Gatos, USA",
   },
   {
-    logo: "https://logo.clearbit.com/adobe.com",
+    logo: "https://tse1.mm.bing.net/th/id/OIP.s2qnKGj8R9kTsbv6IANlEgHaIe?pid=Api&P=0&h=180",
     company: "Adobe",
     deadline: "6 days ago",
     position: "Product Designer",
@@ -76,7 +76,7 @@ function App() {
     location: "San Jose, USA",
   },
   {
-    logo: "https://logo.clearbit.com/intel.com",
+    logo: "https://tse4.mm.bing.net/th/id/OIP.TZu7Xxng5Wk_f95ZZZDafgHaHa?pid=Api&P=0&h=180",
     company: "Intel",
     deadline: "Yesterday",
     position: "Data Analyst",
@@ -86,7 +86,7 @@ function App() {
     location: "Santa Clara, USA",
   },
   {
-    logo: "https://logo.clearbit.com/ibm.com",
+    logo: "https://tse3.mm.bing.net/th/id/OIP.A1i5ZLZ001ud7kVXtWVpYgHaEK?pid=Api&P=0&h=180",
     company: "IBM",
     deadline: "2 weeks ago",
     position: "Cloud Engineer",
@@ -96,7 +96,7 @@ function App() {
     location: "New York, USA",
   },
   {
-    logo: "https://logo.clearbit.com/oracle.com",
+    logo: "https://tse1.mm.bing.net/th/id/OIP.rSQ9DtGcrL8O5zE3Q_wicAHaEK?pid=Api&P=0&h=180",
     company: "Oracle",
     deadline: "3 days ago",
     position: "Java Developer",
@@ -106,15 +106,20 @@ function App() {
     location: "Austin, USA",
   },
 ];
-   console.log(jobs);
+  //  console.log(jobs);
   return (
     <div className='parent'>
 
-   {jobs.map(function(val){
+   {jobs.map(function(val,idx){
+    console.log(idx)
 
-    return <Card name={val.company} logo={val.logo} position={val.position} deadline={val.deadline} tag1={val.duration} tag2={val.level} pay={val.pay} location={val.location} 
+    return <div key={idx}>
+      <Card name={val.company} logo={val.logo} position={val.position} deadline={val.deadline} tag1={val.duration} tag2={val.level} pay={val.pay} location={val.location} 
     />
-   })}
+   </div>
+    
+    
+    })}
    
   
     
